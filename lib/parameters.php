cd676b09461531fd23f35100cdb24b73eb3a3acd
mliz1984 <30932273+mliz1984@users.noreply.github.com>
2018-11-06 16:11:06 -0500

    Update parameters.php

--- lib/parameters.php
+++ lib/parameters.php
@@ -2,8 +2,8 @@
 define('INCLUDE_PATH',str_replace('\\','/',realpath(dirname(__FILE__).'/'))."/");
 define('BASE_PATH',str_replace('\\','/',realpath(dirname(dirname(__FILE__)).'/'))."/");
 
-define('DB_HOST', 'test.quickplus.org');
+define('DB_HOST', '******');
 
-define('DB_NAME', 'admin_test');
-define('DB_USERNAME', 'admin_test');
-define('DB_PASSWORD', 'YANx1984');
+define('DB_NAME', '******');
+define('DB_USERNAME', '******');
+define('DB_PASSWORD', '******');
