312a0937277a7c65d55e5b3d7cfd91c68d2e34c1
mliz1984 <mliz1984@gmail.com>
2018-11-13 13:45:36 -0500

    fix some error include

--- lib/quickform/reg.php
+++ lib/quickform/reg.php
@@ -1,4 +1,3 @@
 <?php 
      require_once($_SERVER['DOCUMENT_ROOT']."/class/rightManager.php");
      require_once($_SERVER['DOCUMENT_ROOT']."/class/menuManager.php");
-     require_once($_SERVER['DOCUMENT_ROOT']."/class/siteManager.php");
